6d37b0c72efc4fcf14e85d52ba9a453eac03aef4
dangthaii <asawholeandlove@gmail.com>
2024-05-28 16:46:09 +0700

    add view engine

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,14 +1,24 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
+  private UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
 
   @GetMapping("/")
-  public String getHomePage() {
-    return "home.html";
+  public String getHomePage(Model model) {
+    String data = userService.getData();
+    model.addAttribute("data", data);
+    return "home";
   }
 
 }
