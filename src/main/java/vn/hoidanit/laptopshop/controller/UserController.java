a4452677129830d93f0bd1f69e2ca89c000acb91
dangthaii <asawholeandlove@gmail.com>
2024-05-28 15:23:30 +0700

    first view and controller

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,14 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+  @GetMapping("/")
+  public String getHomePage() {
+    return "home.html";
+  }
+
+}
