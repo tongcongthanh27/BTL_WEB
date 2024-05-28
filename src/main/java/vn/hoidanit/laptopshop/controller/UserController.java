971a242cc246db5ad56023236eee1c66f3a9276f
dangthaii <asawholeandlove@gmail.com>
2024-05-28 18:06:36 +0700

    transfer data form between view and controller

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -3,7 +3,10 @@ package vn.hoidanit.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
@@ -22,9 +25,15 @@ public class UserController {
   }
 
   @GetMapping("/admin/user")
-
   public String getCreateUserPage(Model model) {
+    model.addAttribute("newUser", new User());
     return "admin/user/create";
+  }
+
+  @PostMapping("/admin/user")
+  public String createUser(Model model, @ModelAttribute User newUser) {
+    System.out.println(newUser);
+    return "home";
   }
 
 }
